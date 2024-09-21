b2b60d13119b7a23e3a04df54614a0a802219f31
RBurkhardt87 <r.burkhardt.ibclc@gmail.com>
2024-09-21 14:44:18 -0500

    Ran from a different computer and got all 4 tests to pass

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -10,6 +10,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+
 /**
  * Created by LaunchCode
  */
@@ -19,6 +20,14 @@ public class JobData {
     private static boolean isDataLoaded = false;
 
     private static ArrayList<HashMap<String, String>> allJobs;
+
+    //TODO: Creating a getter method for allJobs ArrayList
+    //"non-static method .getAllJobs() can not be referenced from a static context."
+    //So, I made this a public static ArrayList...
+    public static ArrayList<HashMap<String, String>> getAllJobs(){
+        return allJobs;
+    }
+
 
     /**
      * Fetch list of all values from loaded data,
@@ -42,6 +51,9 @@ public class JobData {
             }
         }
 
+        // Bonus mission: sort the results
+        Collections.sort(values);
+
         return values;
     }
 
@@ -50,7 +62,8 @@ public class JobData {
         // load data, if not already loaded
         loadData();
 
-        return allJobs;
+        // Bonus mission; normal version returns allJobs
+        return new ArrayList<>(allJobs);
     }
 
     /**
@@ -75,13 +88,15 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
 
         return jobs;
     }
+
+
 
     /**
      * Search all columns for the given term
@@ -95,8 +110,49 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        //We will have an arrayList filled with hashMaps (key/value pairs) We will call it 'searchJobs'
+        //We need to loop over allJobs to look for the search term (value).
+        //So we need to loop over every key and look inside the values of each of the jobs.
+
+        //Case sensitivity, if we were doing equals I could use equalsIgnoreCase, but then it would rule out if the
+        //search term was mentioned in the value. So, I think I would have to just make the value and search term set
+        //to either Upper or Lowercase. I would think this needs to go here for All search & also for searching the
+        //columns as well.
+
+        ArrayList<HashMap<String, String>> searchJobs = new ArrayList<>();
+
+        for (HashMap<String, String> job : allJobs) {
+
+            for (String searchKey : job.keySet()) {
+                if (job.get(searchKey).toLowerCase().contains(value.toLowerCase())) {
+                    if (!searchJobs.contains(job)) {
+                        searchJobs.add(job);
+                    }
+                }
+            }
+        }
+        return searchJobs;
     }
+
+
+    /*
+    This is from the studio counting characters: We looped over each letter
+
+            char[] charactersInString = quote.toUpperCase().toCharArray();
+
+        HashMap<Character, Integer> charactersMap = new HashMap<>();
+
+        for (char letter : charactersInString) {
+
+
+            if (charactersMap.containsKey(letter)) {
+                charactersMap.put(letter, charactersMap.get(letter) + 1);
+            } else {
+                charactersMap.put(letter, 1);
+            }
+
+        }
+     */
 
     /**
      * Read in data from a CSV file and store it in a list
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -59,16 +59,22 @@ public class TechJobs {
 
                 // What is their search term?
                 System.out.println("\nSearch term:");
+
+
+
                 String searchTerm = in.nextLine();
 
+
                 if (searchField.equals("all")) {
-                    System.out.println("Search all fields not implemented yet.");
+                    printJobs(JobData.findByValue(searchTerm));
+
                 } else {
                     printJobs(JobData.findByColumnAndValue(searchField, searchTerm));
                 }
             }
         }
     }
+
 
     // ﻿Returns the key of the selected item from the choices Dictionary
     private static String getUserSelection(String menuHeader, HashMap<String, String> choices) {
@@ -112,14 +118,128 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
-    private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        /*
+         To do this, you’ll need to iterate over an ArrayList of jobs. Each job is itself a HashMap. While you can get
+         each of the items out of the HashMap using the known keys (employer, location, etc.), think instead about
+         creating a nested loop to loop over each HashMap. If a new field is added to the job records, this approach
+         will print out the new field without any updates to printJobs.
+
+        This is how it is printing now...
+        {position type=Data Scientist / Business Intelligence, name=Data Science, employer=Utilidata, location=Rhode Island, core competency=Statistical Analysis}
+
+
+         This is what the jobs should print out as...
+
+                *****
+                position type: Data Scientist / Business Intelligence
+                name: Sr. IT Analyst (Data/BI)
+                employer: Bull Moose Industries
+                location: Saint Louis
+                core competency: Statistical Analysis
+                *****
+
+                *****
+                position type: Web - Back End
+                name: Ruby specialist
+                employer: LaunchCode
+                location: Saint Louis
+                core competency: Javascript
+                *****
+
+            The first for loop is iterating over the ArrayList, but had to nest another for loop to iterate over
+            the hashmaps inside the ArrayList themselves.
+
+            EX: from our studio of counting characters
+            for (Map.Entry<Character, Integer> letter : charactersMap.entrySet()) {
+            System.out.println(letter.getKey() + ": " + letter.getValue());
+
+
+            This is what the No Results should look like:
+
+                Search term:
+                Example Search Term with No Results
+                No Results
+                View jobs by (type 'x' to quit):
+                0 - Search
+                1 - List
+
+            Maybe make an if else statement, if it is true execute the code I currently have
+            Else, write code that will print out NO RESULT message
+
+            Maybe something like if All print what we have
+            else if search term contains 'postition type' or 'location' or whatever other ones there are
+            we can filter through the ArrayList of HashMaps and only print the ones with those values matching the
+            specific search term. If none have a value that matches the search term with the key, then No Results message
+
+
+//This is happening when I press Search and then location
+            Search term:
+cancun
+Does this have anything to do with it?
+*****
+position type: Data Scientist / Business Intelligence
+name: Junior Data Analyst
+employer: Lockerdome
+location: Saint Louis
+core competency: Statistical Analysis
+*****
+
+
+This is happening when I press search and then all
+Search term:
+cancun
+Whats happening here
+*****
+position type: Data Scientist / Business Intelligence
+name: Junior Data Analyst
+employer: Lockerdome
+location: Saint Louis
+core competency: Statistical Analysis
+*****
+
+
+WHAT DOES THAT MEAN? THAT MEANS THAT I NEED TO ADD SOME CONDITIONALS, BUT WHERE??? IN THE PRINTJOBS, CORRECT. IT IS THE
+METHOD 'PRINTJOB' AND RIGHT NOW IT IS SETUP TO ALWAYS DISPLAY ALL THE DATA. I NEED TO WRITE A CONDITION THAT WILL PREVENT
+THE POSITIONS TO ALWAYS DISPLAY. SOMETHING LIKE... IF ARRAY IS EMPTY....
+         */
+
+    //TODO: Create a loop and iterate over the ArrayList of jobs
+    //I probably need to create a getter for allJobs ArrayList so I can retrieve the jobs from it
+    //When I choose List and All it prints out all the jobs with this code. But, when I pick different List choices
+    //it doesn't print. I am assuming I will need to write some conditionals eventually
+
+
+    private static void printJobs(ArrayList<HashMap<String, String>> jobs) {
+
+        if (jobs.isEmpty()) {
+            System.out.print("No Results");
+        } else if (!jobs.isEmpty()) {
+            for (HashMap<String, String> job : jobs) {
+                System.out.println();
+                System.out.println("*****");
+                for (Map.Entry<String, String> item : job.entrySet()) {
+                    System.out.println(item.getKey() + ": " + item.getValue());
+                }
+                System.out.println("*****");
+            }
+
+        } else {
+
+            for (HashMap<String, String> job : JobData.getAllJobs()) {
+                System.out.println("*****");
+                for (Map.Entry<String, String> item : job.entrySet()) {
+                    System.out.println(item.getKey() + ": " + item.getValue());
+                }
+                System.out.println("*****");
+                System.out.println();
+            }
+        }
     }
 }
